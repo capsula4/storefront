65f6b181976c3462a41eee7587e2f5e92545fa91
Pablo Sanchez <luis@vendhq.com>
2017-10-09 22:02:54 +1300

    Add highlight effect when modifying cart

--- app/components/header/header.controller.js
+++ app/components/header/header.controller.js
@@ -1,4 +1,4 @@
-export default /*@ngInject*/ function(ModalService, $state, bcCartService) {
+export default /*@ngInject*/ function(ModalService, $state, $timeout, bcCartService) {
     /**
      * Loads the homepage.
      */
@@ -7,11 +7,30 @@ export default /*@ngInject*/ function(ModalService, $state, bcCartService) {
     };
 
     /**
+     * Highlights the "My Cart" link
+     */
+    this.highlightCart = () => {
+        this.highlight = true;
+
+        $timeout(() => {
+            this.highlight = false;
+        }, 1000);
+    };
+
+    /**
      * Get total quantity of items in cart.
      *
      * @return {Number}
      */
     this.getTotal = () => {
-        return bcCartService.getTotalQuantity();
+        if (this.quantity !== bcCartService.getTotalQuantity()) {
+            if (typeof this.quantity !== 'undefined') {
+                this.highlightCart();
+            }
+
+            this.quantity = bcCartService.getTotalQuantity();
+        }
+
+        return this.quantity;
     };
 }
